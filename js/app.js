499e06b9133a63cb2800a6e9391cb52756a1c666
ryanyun2010 <38802585+ryanyun2010@users.noreply.github.com>
2023-10-11 13:09:21 -0700

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,308 @@
+var startimg;
+var conveyerimgs = [];
+var conveyerimgframe = 0;
+var curframe = 0;
+var currentlyOtherFrame = false;
+var himg, eimg, oimg,lightimg,eeimg,NADPimg,NADPHimg,ATPimg,ADPimg;
+var stage = 2;
+var hs = [];
+var es = [];
+var os = [];
+var ls = [];
+var nadps = [];
+var adps = [];
+function randomNumber(min, max) {
+    return Math.random() * (max - min) + min;
+}
+class Molecule{
+    constructor(img,x,y,s,target){
+        this.x = x;
+        this.y = y;
+        this.s = s;
+        this.target = target;
+        this.img = img;
+    }
+    draw(){
+        image(this.img,this.x,this.y,this.s,this.s)
+    }
+    update(){
+        this.draw();
+        if(Math.hypot(this.x - this.target.x,this.y - this.target.y) > 1){
+            this.move();
+        }
+    }
+    move(){
+        this.angle = atan2((this.target.y) - (this.y), (this.target.x) - (this.x));
+        let xspeed = cos(this.angle) * 1.7;
+        let yspeed = sin(this.angle) * 1.7;
+        this.x += xspeed;
+        this.y += yspeed;
+    }
+}
+class H extends Molecule{
+    constructor(x,y,s,hydrolosis){
+        if(hydrolosis){
+            super(himg,x,y,s,{"x":Math.random() * 700 + 500,"y":Math.random() * 150 + 500})
+        }else{
+            super(himg,x,y,s,{"x":Math.random() * 700 + 500,"y":Math.random() * 150 + 50})
+        }
+        this.lastMoved = 0;
+
+    }
+    update(){
+        this.lastMoved --;
+        this.draw();
+        if(Math.hypot(this.x - this.target.x,this.y - this.target.y) > 1){
+            this.move();
+        }
+        if(this.target.x == 530 && this.target.y == 365 && Math.hypot(this.x - this.target.x,this.y - this.target.y) < 2){
+            console.log("E")
+            this.target = {"x":530,"y":440};
+        }
+        if(this.target.x == 530 && this.target.y == 440 && Math.hypot(this.x - this.target.x,this.y - this.target.y) < 2){
+            console.log("E")
+            this.target = {"x":Math.random() * 700 + 500,"y":Math.random() * 150 + 500};
+        }
+        if(this.y > 400 && Math.hypot(this.target.x - this.x,this.target.y - this.y) < 10){
+            this.target = {"x":1122,"y":500};
+        }
+        if(this.target.x == 1122 && this.target.y == 500 && Math.hypot(this.target.x - this.x,this.target.y - this.y) < 10){
+            this.target = {"x":1112,"y":335};
+            adps.push(new ADP());
+            this.lastMoved = 100000000;
+        }
+        if(this.target.x == 1112 && this.target.y == 335 && Math.hypot(this.target.x - this.x,this.target.y - this.y) < 2){
+            console.log("testttt");
+            for(var adp of adps){
+                if(Math.hypot(this.x - adp.x, this.y - adp.y) < 2 && adp.img == ADPimg){
+                    adp.img = ATPimg;
+                    hs.splice(hs.indexOf(this),1);
+                    adp.target.y -= 1000;
+                    return;
+                }
+            }
+        }
+
+    }
+}
+
+class LightPS1 extends Molecule{
+    constructor(x,y){
+        super(lightimg,x,y,20,{"x":346,"y":410});
+    }
+}
+class LightPS2 extends Molecule{
+    constructor(x,y){
+        super(lightimg,x,y,20,{"x":758,"y":404});
+    }
+    move(){
+        this.angle = atan2((this.target.y) - (this.y), (this.target.x) - (this.x));
+        let xspeed = cos(this.angle) * 1.52;
+        let yspeed = sin(this.angle) * 1.52;
+        this.x += xspeed;
+        this.y += yspeed;
+    }
+}
+
+
+class E extends Molecule{
+    constructor(x,y,s){
+        super(eimg,x,y,s,{"x":346,"y":410})
+    }
+    update(){
+        this.draw();
+        if(Math.hypot(this.x - this.target.x,this.y - this.target.y) > 1){
+            this.move();
+          
+        }
+        for(var l of ls){
+            if(Math.hypot(this.x - l.x,this.y-l.y) < 2 && Math.hypot(this.x - 346,this.y-410) < 2){
+                this.img = eeimg;
+                ls.splice(ls.indexOf(l),1);
+                this.target = {"x":469,"y":404}
+            }
+            if(stage == 2){
+                stage = 3;
+            } 
+            
+        }
+        if(Math.hypot(this.x - 469,this.y - 404) < 2 && this.target.x == 469 && this.target.y == 404){
+            this.target = {"x":758,"y":404}
+            for(var h of hs){
+                if(h.y < 400 && h.lastMoved < 0 && Math.hypot(h.target.x - h.x,h.target.y - h.y) < 10){
+                    h.lastMoved = 500;
+                    h.target = {"x":530,"y":365};
+                    break;
+                }
+            }
+        }
+       
+        
+        if(Math.hypot(this.x - 639,this.y - 404) < 2){
+            this.img = eimg;
+        }
+        if(Math.hypot(this.x - 758, this.y - 404) < 2){
+            if(stage == 3){
+                stage ++;
+            }
+            for(var l of ls){
+                if(Math.hypot(this.x - l.x,this.y-l.y) < 2){
+                    this.img = eeimg;
+                    ls.splice(ls.indexOf(l),1);
+                    this.target.x += 134;
+                    this.target.y += 6;
+                }
+            }
+        }
+        if(Math.hypot(this.x - 892,this.y - 410) < 2){
+            this.target = {"x":960,"y":350}
+        }
+        if(Math.hypot(this.x - 960, this.y - 350) < 2){
+            for(var nadp of nadps){
+                if(Math.hypot(this.x - nadp.x,this.y-nadp.y) < 2){
+                    es.splice(es.indexOf(this),1);
+                    nadp.img = NADPHimg;
+                    nadp.target.y -= 1000;
+                    nadp.speed = 2;
+                }
+            }
+        }
+    }
+    
+}
+
+class O extends Molecule{
+    constructor(x,y,s){
+        super(oimg,x,y,s,{})
+    }
+    move(){
+        this.y -= 5;
+        if(this.y < 0){
+            os.splice(os.indexOf(this),1);
+        }
+    }
+    update(){
+        this.draw();
+        this.move();
+    }
+}
+
+class NADP extends Molecule{
+    constructor(x,y){
+        super(NADPimg,984,-10,30,{"x":960,"y":350});
+        this.speed = 4;
+    }
+    move(){
+        this.angle = atan2((this.target.y) - (this.y), (this.target.x) - (this.x));
+        let xspeed = cos(this.angle) * this.speed;
+        let yspeed = sin(this.angle) * this.speed;
+        this.x += xspeed;
+        this.y += yspeed;
+    }
+}
+class ADP extends Molecule{
+    constructor(x,y){
+        super(ADPimg,1423,360,30,{"x":1112,"y":335});
+        this.speed = 2.7;
+    }
+    move(){
+        this.angle = atan2((this.target.y) - (this.y), (this.target.x) - (this.x));
+        let xspeed = cos(this.angle) * this.speed;
+        let yspeed = sin(this.angle) * this.speed;
+        this.x += xspeed;
+        this.y += yspeed;
+    }
+}
+
+
+
+
+
+
+
+function preload(){
+    startimg = loadImage("../PART1.png");
+    eimg = loadImage("../electron.png");
+    himg = loadImage("../H+.png");
+    oimg = loadImage("../O2.png");
+    lightimg = loadImage("../light.png");
+    eeimg = loadImage("../ee.png");
+    NADPimg = loadImage("../NADP.png");
+    NADPHimg = loadImage("../NADPH.png");
+    ADPimg = loadImage("../ADP.png");
+    ATPimg = loadImage("../ATP.png");
+    for(var i = 1; i < 25; i++){
+        if(i < 10){
+            conveyerimgs.push(loadImage("../conveyerimg/Conveyer-0" +i + ".png"));
+        }else{
+            conveyerimgs.push(loadImage("../conveyerimg/Conveyer-" +i + ".png"));
+        }
+    }
+    conveyerimgs.push(loadImage("../conveyerimg/Conveyer-42.png"));
+    conveyerimgs.push(loadImage("../conveyerimg/Conveyer-43.png"));
+    
+}
+function setup(){
+    createCanvas(1423,703);
+}
+function draw(){
+    background(230);
+    curframe ++;
+    if(stage >= 3){
+        if(curframe % 52 == 0){
+            ls.push(new LightPS2(300,200));
+        }
+    }
+    if(stage >= 2){
+        if(curframe % 52 == 0){
+            ls.push(new LightPS1(300,200));
+        }
+    }
+    if(stage >= 4){
+        if(curframe % 52 == 0){
+            nadps.push(new NADP());
+        }
+    }
+    if(curframe % 52 == 0){
+        hs.push(new H(711.5,2,20,false))
+    }
+
+    currentlyOtherFrame = (currentlyOtherFrame) ? false : true;
+    image(startimg,0,0,1423,703);
+    if(stage >= 1){
+        fill("white");
+        noStroke();
+        image(conveyerimgs[conveyerimgframe],205.5,502,250,125.7)
+        if(currentlyOtherFrame){
+            conveyerimgframe++;
+        }
+        if(conveyerimgframe == conveyerimgs.length - 2 && currentlyOtherFrame){
+            hs.push(new H(426.5,566,20,true))
+            hs.push(new H(398,577,20,true))
+            es.push(new E(416.5,581,16));
+            os.push(new O(377.4,578,20));
+        }
+        if(conveyerimgframe > conveyerimgs.length - 1){
+            conveyerimgframe = 0;
+        }
+    }
+    for(var h of hs){
+        h.update();
+    }
+    for(var e of es){
+        e.update();
+    }
+    for(var o of os){
+        o.update();
+    }
+    for(var l of ls){
+        l.update();
+    }
+    for(var nadp of nadps){
+        nadp.update();
+    }
+    for(var adp of adps){
+        adp.update();
+    }
+}
+
---
+++ js/appCALVIN.js
@@ -0,0 +1,363 @@
+class Molecule{
+    constructor(img,x,y,s,target){
+        this.x = x;
+        this.y = y;
+        this.s = s;
+        this.target = target;
+        this.img = img;
+    }
+    draw(){
+        image(this.img,this.x,this.y,this.s,this.s)
+    }
+    update(){
+        this.draw();
+        if(Math.hypot(this.x - this.target.x,this.y - this.target.y) > 5){
+            this.move();
+        }
+        if(this.img == G3Pimg && this.target.x == 320 && this.target.y == 580.75 && Math.hypot(this.x - this.target.x, this.y - this.target.y) < 9 && combstage >= 1){
+            this.target = {"x":795,"y":580.75};
+        }
+        if(this.img == G3Pimg && this.target.x == 1270 && this.target.y == 580.75 && Math.hypot(this.x - this.target.x, this.y - this.target.y) < 9 && combstage >= 1){
+            this.target = {"x":795,"y":580.75};
+        }
+        if(this.img == G3Pimg && this.target.x == 320 && this.target.y == 556.75 && Math.hypot(this.x - this.target.x, this.y - this.target.y) < 9 && combstage >= 2){
+            this.target = {"x":795,"y":580.75};
+        }
+        if(this.img == G3Pimg && this.target.x == 1270 && this.target.y == 556.75 && Math.hypot(this.x - this.target.x, this.y - this.target.y) < 9 && combstage >= 2){
+            this.target = {"x":795,"y":580.75};
+        }
+        if(this.img == G3Pimg && this.target.x == 795 && this.target.y == 498 && Math.hypot(this.x - this.target.x, this.y - this.target.y) < 9 && combstage >= 3){
+            this.target = {"x":795,"y":556.75};
+        }
+        if(this.img == G3Pimg && this.target.x == 795 && this.target.y == 522 && Math.hypot(this.x - this.target.x, this.y - this.target.y) < 9 && combstage >= 3){
+            this.target = {"x":795,"y":556.75};
+        }
+
+        // TOP RIGHT ENDS AT (1270,498) and (1270,522)
+        if(this.img == G3Pimg && this.target.x == 1270 && this.target.y == 498 && Math.hypot(this.x - this.target.x, this.y - this.target.y) < 9 && combstage >= 4){
+            this.target = {"x":795,"y":556.75};
+        }
+        if(this.img == G3Pimg && this.target.x == 1270 && this.target.y == 522 && Math.hypot(this.x - this.target.x, this.y - this.target.y) < 9 && combstage >= 4){
+            this.target = {"x":795,"y":556.75};
+        }
+
+        
+    }
+    move(){
+        this.angle = atan2((this.target.y) - (this.y), (this.target.x) - (this.x));
+        let xspeed = cos(this.angle) * 3.7;
+        let yspeed = sin(this.angle) * 3.7;
+        this.x += xspeed;
+        this.y += yspeed;
+
+    }
+}
+class NADPH extends Molecule{
+    constructor(x,y,target){
+        super(NADPHimg,x,y,30,target);
+        this.speed = 3.3;
+    }
+    move(){
+        this.angle = atan2((this.target.y) - (this.y), (this.target.x) - (this.x));
+        let xspeed = cos(this.angle) * this.speed;
+        let yspeed = sin(this.angle) * this.speed;
+        this.x += xspeed;
+        this.y += yspeed;
+    }
+}
+class ATP extends Molecule{
+    constructor(x,y,target){
+        super(ATPimg,x,y,30,target);
+        this.speed = 3.3;
+    }
+    move(){
+        this.angle = atan2((this.target.y) - (this.y), (this.target.x) - (this.x));
+        let xspeed = cos(this.angle) * this.speed;
+        let yspeed = sin(this.angle) * this.speed;
+        this.x += xspeed;
+        this.y += yspeed;
+    }
+}
+
+class O extends Molecule{
+    constructor(x,y,s){
+        super(oimg,x,y,s,{})
+    }
+    move(){
+        this.y -= 5;
+        if(this.y < 0){
+            molecules.splice(molecules.indexOf(this),1);
+        }
+    }
+    update(){
+        this.draw();
+        this.move();
+    }
+}
+
+
+
+
+var startimg,rubpimg,co2img,rubp1img,RuBisCOimg,G3Pimg,glucoseimg,G3P1img,oimg;
+var stage = 1;
+var combstage = 0;
+var framecount = 0;
+var molecules = [];
+function preload(){
+    rubpimg = loadImage("../RUBP.png");
+    startimg = loadImage("../CALVIN.png");
+    co2img = loadImage("../CO2.png");
+    rubp1img = loadImage("../RUBP+CARBON.png");
+    RuBisCOimg = loadImage("../RuBisCo.png");
+    NADPHimg = loadImage("../NADPH.png");
+    ATPimg = loadImage("../ATP.png");
+    G3Pimg = loadImage("../G3P.png");
+    glucoseimg = loadImage("../Glucose.png")
+    G3P1img = loadImage("../G3P+1.png");
+    oimg = loadImage("../O2.png");
+}
+function setup(){
+    createCanvas(1423,703);
+    imageMode(CENTER);
+    rectMode(CENTER);
+}
+function draw(){
+    
+    background(230);
+    image(startimg,1423/2,703/2,1423,703);
+    if(framecount % 800 == 0){
+        // TOP LEFT ENDS AT (320,498) and (320,522)
+        if(framecount < 800) {molecules.push(new Molecule(rubpimg,120,0,40,{"x":120,"y":120}));}
+        molecules.push(new Molecule(co2img,0,120,20,{"x":120,"y":120}));
+        molecules.push(new Molecule(RuBisCOimg,0,0,30,{"x":120,"y":120}));
+
+        // TOP MID ENDS AT (795,498) and (795,522)
+        if(framecount < 800) {molecules.push(new Molecule(rubpimg,120 + 475,0,40,{"x":120 + 475,"y":120}));}
+        molecules.push(new Molecule(co2img,0 + 475,120,20,{"x":120 + 475,"y":120}));
+        molecules.push(new Molecule(RuBisCOimg,475,0,30,{"x":120 + 475,"y":120}));
+
+        // TOP RIGHT ENDS AT (1270,498) and (1270,522)
+        if(framecount < 800) {molecules.push(new Molecule(rubpimg,120 + 475 * 2,0,40,{"x":120 + 475 * 2,"y":120}));}
+        molecules.push(new Molecule(co2img,0 + 475 * 2,120,20,{"x":120 + 475 * 2,"y":120}));
+        molecules.push(new Molecule(RuBisCOimg,475 * 2,0,30,{"x":120 + 475 * 2,"y":120}));
+
+        // BOT LEFT ENDS AT (320,556.75) and (320,580.75)
+        if(framecount < 800) { molecules.push(new Molecule(rubpimg,120,0 + 235,40,{"x":120,"y":120 + 235}));}
+        molecules.push(new Molecule(co2img,0,120 + 235,20,{"x":120,"y":120 + 235}));
+        molecules.push(new Molecule(RuBisCOimg,0,235,30,{"x":120,"y":120 + 235}));
+
+        // BOT MID ENDS AT (795,556.75) and (795,580.75)
+        if(framecount < 800) {molecules.push(new Molecule(rubpimg,120 + 475,0 + 235,40,{"x":120 + 475,"y":120 + 235}));}
+        molecules.push(new Molecule(co2img,475,120 + 235,20,{"x":120 + 475,"y":120 + 235}));
+        molecules.push(new Molecule(RuBisCOimg,475,235,30,{"x":120 + 475,"y":120 + 235}));
+
+        // BOT RIGHT ENDS AT (1270,556.75) and (1270,580.75)
+        if(framecount < 800) {molecules.push(new Molecule(rubpimg,120 + 475 * 2,0 + 235,40,{"x":120 + 475 * 2,"y":120 + 235}));}
+        molecules.push(new Molecule(co2img,475 * 2,120 + 235,20,{"x":120 + 475 * 2,"y":120 + 235}));
+        molecules.push(new Molecule(RuBisCOimg,475 * 2,235,30,{"x":120 + 475 * 2,"y":120 + 235}));
+    }
+    if((framecount + 660) % 800 == 0 && (framecount + 670) > 800){
+        // TOP LEFT
+        molecules.push(new ATP(200,0,{"x":300,"y":120}));
+        molecules.push(new NADPH(400,0,{"x":300,"y":120}));
+        
+        // TOP MID
+        molecules.push(new ATP(200 + 475,0,{"x":300 + 475,"y":120}));
+        molecules.push(new NADPH(400 + 475,0,{"x":300 + 475,"y":120}));
+
+        // TOP RIGHT
+        molecules.push(new ATP(200 + 475 * 2,0,{"x":300 + 475 * 2,"y":120}));
+        molecules.push(new NADPH(400 + 475 * 2,0,{"x":300 + 475 * 2,"y":120}));
+
+        // BOT LEFT
+        molecules.push(new ATP(200,235,{"x":300,"y":120 + 235}));
+        molecules.push(new NADPH(400,235,{"x":300,"y":120 + 235}));
+
+        // BOT MID
+        molecules.push(new ATP(200 + 475,235,{"x":300 + 475,"y":120 + 235}));
+        molecules.push(new NADPH(400 + 475,235,{"x":300 + 475,"y":120 + 235}));
+
+        // BOT RIGHT
+        molecules.push(new ATP(200 + 475 * 2,235,{"x":300 + 475 * 2,"y":120 + 235}));
+        molecules.push(new NADPH(400 + 475 * 2,235,{"x":300 + 475 * 2,"y":120 + 235}));
+    }
+    for(var molecule of molecules){
+        molecule.update();
+    }
+    for(var molecule of molecules){
+        if(molecule.img == co2img){
+            for(var molecule2 of molecules){
+                if(molecule2.img == rubpimg){
+                    if(Math.hypot(molecule.x - molecule2.x,molecule.y - molecule2.y) < 9){
+                        for(var molecule3 of molecules){
+                            if(Math.hypot(molecule.x - molecule3.x, molecule.y - molecule3.y) < 9 && molecule3.img == RuBisCOimg){
+                                molecule2.img = rubp1img;
+                                molecules.splice(molecules.indexOf(molecule),1);
+                                molecules.splice(molecules.indexOf(molecule3),1);
+                                molecules.push(new O(molecule.x,molecule.y,20))
+                                molecule2.target.x += 180;
+                                molecule2.s = 48;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    for(var molecule of molecules){
+        if(molecule.img == rubp1img){
+            for(var molecule2 of molecules){
+                if(molecule2.img == NADPHimg){
+                    if(Math.hypot(molecule.x - molecule2.x,molecule.y - molecule2.y) < 9){
+                        for(var molecule3 of molecules){
+                            if(Math.hypot(molecule.x - molecule3.x, molecule.y - molecule3.y) < 9 && molecule3.img == ATPimg){
+                                molecule.img = G3Pimg;
+                                molecules.splice(molecules.indexOf(molecule2),1);
+                                molecules.splice(molecules.indexOf(molecule3),1);
+                                molecule.target.x += 20;
+                                molecule.target.y /= 4;
+                                molecule.target.y += 480 - 12;
+                                molecule.s = 24;
+                                molecules.push(new Molecule(G3Pimg,molecule.x,molecule.y + 12,24,{"x":molecule.target.x,"y":molecule.target.y + 24}))
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    for(var molecule of molecules){
+        if(molecule.img == G3Pimg && molecule.target.x == 320 && molecule.target.y == 498){
+            var index = molecules.indexOf(molecule);
+            for(var molecule2 of molecules){
+                if(molecule2.img == G3Pimg && index != molecules.indexOf(molecule2) && molecule2.target.x == 320 && molecule2.target.y == 522){
+                    if(Math.hypot(molecule.x - molecule2.x,molecule.y - molecule2.y) < 30 && Math.hypot(molecule.x - 320, molecule.y - 498) < 9 ){
+                        molecule.target = {"x":320, "y":522};
+                    }
+                }
+            }
+        }
+    }
+    for(var molecule of molecules){
+        if(molecule.img == G3Pimg && molecule.target.x == 320 && molecule.target.y == 522){
+            var index = molecules.indexOf(molecule);
+            for(var molecule2 of molecules){
+                if(molecule2.img == G3Pimg && index != molecules.indexOf(molecule2) && molecule2.target.x == 320 && molecule2.target.y == 522){
+                    if(Math.hypot(molecule.x - molecule2.x,molecule.y - molecule2.y) < 9 && Math.hypot(molecule.x - 320, molecule.y - 522) < 9 ){
+                        molecule.img = glucoseimg;
+                        molecule.s = 48;
+                        molecules.splice(molecules.indexOf(molecule2),1);
+                        molecule.target.x -= 1000;
+                        if(combstage == 0){
+                            combstage = 1;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    for(var molecule of molecules){
+        if(molecule.img == G3Pimg && molecule.target.x == 795 && molecule.target.y == 580.75 && combstage >= 0){
+            var index = molecules.indexOf(molecule);
+            for(var molecule2 of molecules){
+                if(molecule2.img == G3Pimg && index != molecules.indexOf(molecule2) && molecule2.target.x == 795 && molecule2.target.y == 580.75){
+                    var index2 = molecules.indexOf(molecule2);
+                    for(var molecule3 of molecules){
+                        if(molecule3.img == G3Pimg && molecules.indexOf(molecule3) != index2 && molecules.indexOf(molecule3) != index && molecule3.target.x == 795 && molecule3.target.y == 580.75)
+                            if(Math.hypot(molecule.x - molecule2.x,molecule.y - molecule2.y) < 9 && Math.hypot(molecule.x - 795, molecule.y - 580.75) < 9 && Math.hypot(molecule.x - molecule3.x,molecule.y - molecule3.y) < 9){
+                                molecule.img = rubpimg;
+                                molecule.s = 40;
+                                molecules.splice(molecules.indexOf(molecule2),1);
+                                molecule.target = {"x":120,"y":120};
+                                molecule3.img = G3P1img;
+                                molecule3.s = 26 + 2/3;
+                                if(combstage < 2){
+                                    combstage = 2;
+                                }
+                            }
+                    }
+                }
+            }
+        }
+    }
+    for(var molecule of molecules){
+        if(molecule.img == G3Pimg && molecule.target.x == 795 && molecule.target.y == 580.75 && combstage >= 0){
+            var index = molecules.indexOf(molecule);
+            for(var molecule2 of molecules){
+                if(molecule2.img == G3Pimg && index != molecules.indexOf(molecule2) && molecule2.target.x == 795 && molecule2.target.y == 580.75){
+                    var index2 = molecules.indexOf(molecule2);
+                    for(var molecule3 of molecules){
+                        if(molecule3.img == G3P1img && molecules.indexOf(molecule3) != index2 && molecules.indexOf(molecule3) != index && molecule3.target.x == 795 && molecule3.target.y == 580.75)
+                            if(Math.hypot(molecule.x - molecule2.x,molecule.y - molecule2.y) < 9 && Math.hypot(molecule.x - 795, molecule.y - 580.75) < 9 && Math.hypot(molecule.x - molecule3.x,molecule.y - molecule3.y) < 9){
+                                molecule.img = rubpimg;
+                                molecule.s = 40;
+                                molecules.splice(molecules.indexOf(molecule3),1);
+                                molecule.target = {"x":120 + 475,"y":120};
+                                molecule2.img = rubpimg;
+                                molecule2.s = 40;
+                                molecule2.target = {"x":120 + 475 + 475, "y":120};
+                                if(combstage < 3){
+                                    combstage = 3;
+                                }
+                            }
+                    }
+                }
+            }
+        }
+    }
+
+    for(var molecule of molecules){
+        if(molecule.img == G3Pimg && molecule.target.x == 795 && molecule.target.y == 556.75 && combstage >= 3){
+            var index = molecules.indexOf(molecule);
+            for(var molecule2 of molecules){
+                if(molecule2.img == G3Pimg && index != molecules.indexOf(molecule2) && molecule2.target.x == 795 && molecule2.target.y == 556.75){
+                    var index2 = molecules.indexOf(molecule2);
+                    for(var molecule3 of molecules){
+                        if(molecule3.img == G3Pimg && molecules.indexOf(molecule3) != index2 && molecules.indexOf(molecule3) != index && molecule3.target.x == 795 && molecule3.target.y == 556.75)
+                            if(Math.hypot(molecule.x - molecule2.x,molecule.y - molecule2.y) < 9 && Math.hypot(molecule.x - 795, molecule.y - 556.75) < 9 && Math.hypot(molecule.x - molecule3.x,molecule.y - molecule3.y) < 9){
+                                molecule.img = rubpimg;
+                                molecule.s = 40;
+                                molecules.splice(molecules.indexOf(molecule2),1);
+                                molecule.target = {"x":120,"y":120 + 235};
+                                molecule3.img = G3P1img;
+                                molecule3.s = 26 + 2/3;
+                                if(combstage < 4){
+                                    combstage = 4;
+                                }
+                            }
+                    }
+                }
+            }
+        }
+    }
+    
+    for(var molecule of molecules){
+        if(molecule.img == G3Pimg && molecule.target.x == 795 && molecule.target.y == 556.75 && combstage >= 4){
+            var index = molecules.indexOf(molecule);
+            for(var molecule2 of molecules){
+                if(molecule2.img == G3Pimg && index != molecules.indexOf(molecule2) && molecule2.target.x == 795 && molecule2.target.y == 556.75){
+                    var index2 = molecules.indexOf(molecule2);
+                    for(var molecule3 of molecules){
+                        if(molecule3.img == G3P1img && molecules.indexOf(molecule3) != index2 && molecules.indexOf(molecule3) != index && molecule3.target.x == 795 && molecule3.target.y == 556.75)
+                            if(Math.hypot(molecule.x - molecule2.x,molecule.y - molecule2.y) < 9 && Math.hypot(molecule.x - 795, molecule.y - 556.75) < 9 && Math.hypot(molecule.x - molecule3.x,molecule.y - molecule3.y) < 9){
+                                molecule.img = rubpimg;
+                                molecule.s = 40;
+                                molecules.splice(molecules.indexOf(molecule3),1);
+                                molecule.target = {"x":120 + 475,"y":120 + 235};
+                                molecule2.img = rubpimg;
+                                molecule2.s = 40;
+                                molecule2.target = {"x":120 + 475 + 475, "y":120 + 235};
+                                if(combstage){
+                                    combstage = 1;
+                                }
+                            }
+                    }
+                }
+            }
+        }
+    }
+
+    framecount++;
+}
+
